19c3d8b53c431cfa0c45e3fc3cab61e6de14078f
Zintan <zintanomar@gmail.com>
2020-06-02 19:02:11 +0000

    remove dead code from doGet()

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -42,8 +42,6 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String commentsString = convertToJsonWithGson(comments);
-    // response.setContentType("text/html;");
-    // response.getWriter().println("Hello Zintan!");
     response.getWriter().println(commentsString);
   }
 
