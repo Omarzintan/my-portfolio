45b799a9c07bd08b181e8710f8d022df9054fc11
Zintan <zintanomar@gmail.com>
2020-06-03 00:55:28 +0000

    add timestamp to Comment class

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -18,9 +18,11 @@ package com.google.sps.data;
 public final class Comment {
   private final long id;
   private final String text;
+  private final long timestamp;
 
-  public Comment(long id, String text) {
+  public Comment(long id, String text, long timestamp) {
     this.id = id;
     this.text = text;
+    this.timestamp = timestamp;
   }
 }
