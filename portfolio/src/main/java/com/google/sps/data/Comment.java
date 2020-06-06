dc9ab1390711bd6c8d8eee42f6704d0d415c0720
Zintan <zintanomar@gmail.com>
2020-06-06 01:59:57 +0000

    add username field in comment entity

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -4,11 +4,13 @@ package com.google.sps.data;
 /** A comment in the list of comments. */
 public final class Comment {
   private final long id;
+  private final String username;
   private final String text;
   private final long timestamp;
 
-  public Comment(long id, String text, long timestamp) {
+  public Comment(long id, String username, String text, long timestamp) {
     this.id = id;
+    this.username = username;
     this.text = text;
     this.timestamp = timestamp;
   }
